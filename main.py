b614551df5b83587d04ac8ad95719015071035a4
SlavaBotnarenku <botnarenkuslava@gmail.com>
2026-02-22 20:58:29 +0300

    setting up project

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+posts: list[dict] = [
+    {
+        "id" : 1,
+        "author": "Slava",
+        "title": "FastApi is Awesome",
+        "content": "this framewor is really easy to use and super fast",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jack Doe",
+        "title": "Python is great for web developmet",
+        "content": "Python is a great language for web development, and fastapi make it even better",
+        "date_posted": "April 21, 2025",
+    }
+]
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+def home():
+    return f"<h1>{posts[0]['title']}</h1>"
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
