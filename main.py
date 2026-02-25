867ad93e4daa548aadf1f613f5e057eab0bce050
SlavaBotnarenku <botnarenkuslava@gmail.com>
2026-02-25 22:34:24 +0300

    added jinja layouts

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ posts: list[dict] = [
 @app.get("/posts", include_in_schema=False)
 @app.get("/", include_in_schema=False)
 def home(request: Request):
-    return templates.TemplateResponse(request, 'home.html', {"posts": posts})
+    return templates.TemplateResponse(request, 'home.html', {"posts": posts, "title": "Главная страница"})
 
 @app.get("/api/posts")
 def get_posts():
